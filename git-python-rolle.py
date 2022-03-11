1256d79ce6dde3f52ec05f63113999f33d620180
RolleJohnJacob <john.jacob.rolle15@gmail.com>
2022-03-12 01:47:12 +0800

    Myhobbies added

--- git-python-rolle.py
+++ git-python-rolle.py
@@ -11,3 +11,13 @@ course = print("Bachelor of Sceince Information Technology")
 
 Section = print("2D")
 
+Myhobby1 = print("Listening to the music")
+
+Myhobby2 = print("Playing Badminton")
+
+Myhobby3 = print("Playing volleyball")
+
+Myhobby4 = print("Playing Games in PC")
+
+Myhobby5 = print("Doing Household Chores")
+
