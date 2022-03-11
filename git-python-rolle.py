c859eb2c9f84d3e8f92541e7eab504f9b6fe6c88
RolleJohnJacob <john.jacob.rolle15@gmail.com>
2022-03-12 01:42:56 +0800

    My Educational Background added

--- git-python-rolle.py
+++ git-python-rolle.py
@@ -5,3 +5,9 @@ address = print("Lamesa Santa Cruz Marinduque")
 Birtday = print("July, 14 2002")
 
     
+college = print("Marinduqe State College")
+
+course = print("Bachelor of Sceince Information Technology")
+
+Section = print("2D")
+
